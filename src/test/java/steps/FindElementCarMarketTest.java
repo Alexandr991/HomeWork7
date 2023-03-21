fa3a44295f13e9ce5e79ca6c0204fb741f863270
Milevskiy <ajleksandr991@gmail.com>
2023-03-21 22:30:21 +0300

    Second Commit

--- src/test/java/steps/FindElementCarMarketTest.java
+++ src/test/java/steps/FindElementCarMarketTest.java
@@ -4,11 +4,12 @@ package steps;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.assertj.core.api.Assertions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.actions;
@@ -16,7 +17,7 @@ import static com.codeborne.selenide.Selenide.actions;
 public class FindElementCarMarketTest {
     private final SelenideElement carMarket = $x("//span[@class='b-main-navigation__text' and text()='Автобарахолка']");
 
-    @And("Наводим курсор на пункт Автобарахолка")
+    @When("Наводим курсор на пункт Автобарахолка")
     public void FindElement() {
         actions().moveToElement(carMarket).perform();
     }
@@ -26,7 +27,7 @@ public class FindElementCarMarketTest {
         ArrayList<String> itemsInMenu = new ArrayList<>();
         itemsInMenu.add("Цена с НДС");
         itemsInMenu.add("Авто до 4000 р.");
-        itemsInMenu.add("Авто до 10000 р.");
+        itemsInMenu.add("Авто до 10 000 р.");
         itemsInMenu.add("Минск");
         itemsInMenu.add("Гомель");
         itemsInMenu.add("Могилев");
@@ -40,6 +41,12 @@ public class FindElementCarMarketTest {
         itemsInMenu.add("Mazda");
         itemsInMenu.add("Mercedes-Benz");
         ElementsCollection items = Selenide.$$x("//a[text()='Автобарахолка']/../..//div[contains(@class, 'grid')]//span[contains(@class, 'sign')]");
-        Assertions.assertThat(items.containsAll(itemsInMenu)).isTrue();
+        List<String> list = new ArrayList<>();
+        for (SelenideElement x : items
+        ) {
+            list.add(x.getOwnText());
+        }
+
+        Assertions.assertThat(list.containsAll(itemsInMenu)).isTrue();
     }
 }
--- src/test/java/steps/FindElementFlatAndHouseTest.java
+++ src/test/java/steps/FindElementFlatAndHouseTest.java
@@ -3,11 +3,12 @@ package steps;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.assertj.core.api.Assertions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.actions;
@@ -15,7 +16,7 @@ import static com.codeborne.selenide.Selenide.actions;
 public class FindElementFlatAndHouseTest {
     private final SelenideElement flatAndHouse = $x("//span[@class='b-main-navigation__text' and text()='Дома и квартиры']");
 
-    @And("Наводим курсор на пункт Дома и квартиры")
+    @When("Наводим курсор на пункт Дома и квартиры")
     public void findElement() {
         actions().moveToElement(flatAndHouse).perform();
     }
@@ -37,6 +38,11 @@ public class FindElementFlatAndHouseTest {
         itemsInMenu.add("30 000–80 000 $");
         itemsInMenu.add("От 80 000 $");
         ElementsCollection items = Selenide.$$x("//span[text()='Дома и квартиры']/../..//div[contains(@class, 'grid')]//span[contains(@class, 'sign')]");
-        Assertions.assertThat(items.containsAll(itemsInMenu)).isTrue();
+        List<String> list = new ArrayList<>();
+        for (SelenideElement x: items
+        ) {
+            list.add(x.getOwnText());
+        }
+        Assertions.assertThat(list.containsAll(itemsInMenu)).isTrue();
     }
 }
